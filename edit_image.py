edc17b3a75f6e372ea96fda6d87b4ddb71ca30ff
CSY <creightonstanleyyeo@gmail.com>
2023-08-29 12:54:04 +0800

    change direction of rotation and initiated path variable

--- edit_image.py
+++ edit_image.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import os
 
+path = os.getcwd()
+
 for photo in os.listdir("images"):
-    im = Image.open("{}\\images\\{}".format(os.getcwd(),photo))
-    im.rotate(90).show()
+    im = Image.open("{}\\images\\{}".format(path,photo))
+    im.rotate(270).show()
